864ee6272a8ccb9cf1a560efadf912145c19e69c
Nathanael Rachmat <19623100@std.stei.itb.ac.id>
2024-10-09 14:49:54 +0700

    feat: matriks kofaktor

--- Matrix/src/Kofaktor.java
+++ Matrix/src/Kofaktor.java
@@ -14,17 +14,28 @@ public class Kofaktor {
 		}
 	}
 	
+	public static float[][] MatriksKofaktor(float[][] m){
+		float[][] m2 = new float[OBE.getRowEff(m)][OBE.getColEff(m)];
+		for (int i = 0; i < OBE.getRowEff(m);i++ ) {
+			for (int j = 0; j < OBE.getColEff(m);j++ ) {
+				m2[i][j] = kofaktor(m,i,j);
+			}
+		}
+		return m2;
+	}
+	
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		float[][] theo = {
-				{3,-1,2},
-				{5,0,4},
-				{8,2,-3},
+				{3,2,-1},
+				{1,6,3},
+				{2,-4,0},
 		};
 		float C00 = Determinan.DeterminanKofaktor(theo);
 		System.out.println(C00);
 		float C = kofaktor(theo,0,1);
 		System.out.println(C);
+		theo = MatriksKofaktor(theo);
+		OBE.printMatrix(theo);
 	}
-
 }
